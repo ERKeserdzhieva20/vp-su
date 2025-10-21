332ae738ad9a5803f493cf8928e1895e35929c78
Elena Keserdzhieva <85404905+ERKeserdzhieva20@users.noreply.github.com>
2025-10-21 11:17:50 +0300

    Do exercises in https://classroom.google.com/c/ODE3OTQ1NTM1ODI0/p/ODE3OTQ4MzM2NjMy/details - class 1

---
+++ PyCharmMiscProject/script.py
@@ -0,0 +1,76 @@
+#1
+# print("hello world")
+
+#2
+# # a = input()
+# # b = input()
+#
+# a = 1;
+# b = 2;
+# print("a + b =", a+b)
+# print("a - b =",a - b)
+# print("a * b =",a * b)
+# print("a / b =",a / b)
+# print("a // b =",a // b)
+# print("a % b =",a % b)
+# print("stiga")
+
+# 3
+# str1 = input("enter a string")
+# str2 = input("enter a string")
+#
+# print("Hello " + "World")
+
+# 4
+# arr1 = [1, 2, 3, 4, 5]
+# arr2 = [6, 7, 8, 9, 10]
+#
+# arr1 += arr2
+#
+# print(arr1)
+
+# 5
+# arr = input().split()
+# n = int(input())
+# for i in range(n):
+#     print(arr)
+
+# 6
+# letter = input('Enter a letter: ')
+# word = input('Enter a word: ')
+# print(letter in word)
+
+# 7
+# student = {
+#   "name": input("Enter your name: "),
+#   "age": input("Enter your age: "),
+#   "speciality": input("Enter your speciality: "),
+# }
+#
+# print(student.get('name'))
+# student["age"] = input("Enter your age: ")
+# student["city"] = input("city")
+# print(student)
+
+#8
+# a = int(input('Enter a number: '))
+# b = int(input('Enter another number: '))
+# s = input('Enter a measurement: ')
+# print(a * b, s)
+# print(f'a = a; b = b; area = {a * b}')
+
+#upr1
+# a = int(input())
+# b = int(input())
+# h = int(input())
+# print(f"{((a + b) * h / 2):.2f}")
+
+#upr2
+# r = float(input())
+# print(f"{2 * r * 3.14:.3f}")
+# print(f"{r * r * 3.14:.3f}")
+
+#upr3
+# hours = int(input("Enter hours: "))
+# rate = float(input("Enter rate: "))
+# print(f"Pay: {hours * rate:.2f}")
